3e84183494e5114e0af24cdeef249b6a6a0602db
cpp <vanshdubey101963@gmail.com>
2024-10-28 00:18:55 +0530

    did some code modulation for resusablity

--- components/ChatList.jsx
+++ components/ChatList.jsx
@@ -1,33 +1,96 @@
-import React from 'react'
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import { Box , Avatar } from '@mui/material';
+import React from "react";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { Box, Avatar, Stack } from "@mui/material";
+import Divider from "@mui/material/Divider";
 
 const ChatList = (props) => {
-  const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+  const [selectedIndex, setSelectedIndex] = React.useState();
 
   const handleListItemClick = (event, index) => {
     setSelectedIndex(index);
   };
-  return (
-    <Box sx={{backgroundColor: props.theme.palette.background.paper , height: "95vh", width: 360 , borderRadius: 1.5 , outline: 'none'}}>
-                
-          <List>
-          <ListItemButton
-            selected={selectedIndex === 0}
-            onClick={(event) => handleListItemClick(event, 0)}
-            sx={{borderRadius: 1.5 , gap: 2}}
-          >
-            <ListItemIcon>
-            <Avatar alt='' src='src/assets/app-icon.png' sx={{p:1, backgroundColor: '#0572F4'}} />
-            </ListItemIcon>
-            <ListItemText primary="Vansh Dubey" />
-          </ListItemButton>
-          </List>
-        </Box>
-  )
-}
 
-export { ChatList }
+  return (
+    <Box 
+      sx={{
+        backgroundColor: '#F8F9FF',
+        height: "95vh",
+        width: 360,
+        borderRadius: 1.5,
+        outline: "none",
+        padding: 3,
+      }}
+    >
+      <Stack spacing={2}>
+      <h1>Chats</h1>
+      <Divider component={"h1"} />
+      <List>
+        {ChatData.map((el) => (
+            <Chats  key={el.index} name={el.name} index={el.index} iconSrc={el.iconSrc} selectedIndex={selectedIndex}
+            onListItemClick={handleListItemClick} about={el.about} />
+        ))}
+      </List>
+      </Stack>
+    </Box>
+  );
+};
+
+const Chats = (props) => {
+  const index = props.index;
+  const name = props.name;
+  const iconSrc = props.iconSrc;
+  const about = props.about;
+  
+
+  return (
+    <ListItemButton
+      selected={props.selectedIndex === index}
+      onClick={(event) => props.onListItemClick(event, index)}
+      sx={{ borderRadius: '5px', gap: 2 , backgroundColor: "white", marginBottom: 2 , padding: 0}}
+    >
+      <ListItemIcon>
+        <Avatar alt="" src={iconSrc} sx={{ p: 1}} />
+      </ListItemIcon>
+      <ListItemText primary={name} secondary={about}/>
+    </ListItemButton>
+  );
+};
+
+const ChatData = [
+  {
+    index: 0,
+    name: "Vansh Dubey",
+    iconSrc: "src/assets/man_4140048.png",
+    about: 'Otaku in disguise'
+  },
+  {
+    index: 1,
+    name: "Viral Jain",
+    iconSrc: "src/assets/man_4140048.png",
+    about: ''
+  },
+  {
+    index: 2,
+    name: "Sanyam Bhandari",
+    iconSrc: "src/assets/man_4140048.png",
+    about: `for this road's mine, and mine alone..`
+  },
+  {
+    index: 3,
+    name: "Samar Kumar Mandloi",
+    iconSrc: "src/assets/man_4140048.png",
+    about: 'Samar Mandloi'
+  },
+  {
+    index: 4,
+    name: "Yug Shrivastava",
+    iconSrc: "src/assets/man_4140048.png",
+    about: 'A leap of faith'
+  },
+];
+
+export { ChatList };
--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,42 @@
 import { useTheme } from '@mui/material/styles';
-import { Box , Divider , Stack , Avatar , List , ListItemButton} from '@mui/material';
+import { Box , Stack , Avatar , IconButton , Divider} from '@mui/material';
 import { ChatList } from '../components/ChatList';
-import { useState } from 'react';
-
+import { ChatCircleDots , UsersThree , Phone , Gear} from 'phosphor-react';
 
 function App() {
 
   const theme = useTheme()
-  const [selectedIndex, setSelectedIndex] = useState(1);
 
-  const handleListItemClick = (event, index) => {
-    setSelectedIndex(index);
-  };
+  const NavbarIcons = [
+    {
+     index : 0 ,
+     icon : <ChatCircleDots/>,
+    },
+
+    {
+      index : 1,
+      icon : <UsersThree/>,
+    },
+
+    {
+      index : 2,
+      icon : <Phone/>
+    },
+  ]
   
   return (
     <>
-    <Stack direction={'row'}>
-        <Box p={2} m={1} sx={{backgroundColor: theme.palette.background.paper ,height: "95vh",  boxShadow: "0px 0px 2px rgba(0,0,0,0.25)" ,width: 70, borderRadius: 1.5}}>
+    <Stack direction={'row'} m={1} gap={0.1}>
+        <Box p={2} sx={{backgroundColor: '#EEF3FA' ,height: "95vh",  boxShadow: "0px 0px 2px rgba(0,0,0,0.25)" ,width: 70, borderRadius: 1.5}}>
           <Stack direction={'column'} alignItems={'center'} gap={3}>
-              <List>
-              <ListItemButton >
-              <Avatar alt='' src='src/assets/app-icon.png' sx={{p:1, backgroundColor: '#0572F4'}} />
-            </ListItemButton>
-            <ListItemButton
-                 selected={selectedIndex === 1}
-                 onClick={(event) => handleListItemClick(event, 1)}
-              >
-              <Avatar alt='' src='src/assets/communication_13787501.png' sx={{p:1}}/>
-            </ListItemButton> 
-            <ListItemButton
-                 selected={selectedIndex === 2}
-                 onClick={(event) => handleListItemClick(event, 2)}
-              >
-              <Avatar alt='' src='src/assets/group_15804694.png' sx={{p:1}}/>
-            </ListItemButton> 
-            <ListItemButton
-                 selected={selectedIndex === 3}
-                 onClick={(event) => handleListItemClick(event, 3)}
-              >
-               <Avatar alt='' src='src/assets/telephone_14318132.png' sx={{p:1}}/>
-            </ListItemButton>   
-            
-              </List>
+                <Avatar alt='' src='src/assets/app-icon.png' sx={{p:1, backgroundColor: '#0572F4'}} />
+                {NavbarIcons.map( el => (
+                     <IconButton key={el.index}>
+                       {el.icon}
+                    </IconButton>
+                  ))}
+                <Divider variant="middle" flexItem/>
+                <IconButton><Gear/></IconButton>
           </Stack>
         </Box>
         <ChatList theme={theme}/>
